029a55156b22d56f6739a74dc0dd8379ab011eab
Jeroen Heijmans <github@jeroenheijmans.nl>
2020-05-05 19:04:34 +0200

    Move IDS4 config to own file

---
+++ SampleAuthPolicies/IdentityServerConfig.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using IdentityServer4.Models;
+using IdentityServer4.Test;
+
+namespace SampleAuthPolicies
+{
+    public static class IdentityServerConfig
+    {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("foo-api")
+            {
+                Scopes =
+                {
+                    new Scope("foo-api.with.roles", new[] { "role" }),
+                }
+            }
+        };
+
+        public static IEnumerable<Client> Clients => new[]
+        {
+            new Client
+            {
+                // Don't use RPO if you can prevent it. We use it here
+                // because it's the easiest way to demo with users.
+                ClientId = "legacy-rpo",
+                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+                AllowAccessTokensViaBrowser = false,
+                RequireClientSecret = false,
+                AllowedScopes = { "foo-api", "foo-api.with.roles" },
+            },
+        };
+
+        public static List<TestUser> TestUsers => new List<TestUser>
+               {
+                   new TestUser
+                   {
+                       SubjectId = "ABC-123",
+                       Username = "john",
+                       Password = "secret",
+                       Claims = { new Claim("role", "user") },
+                   },
+                   new TestUser
+                   {
+                       SubjectId = "ABC-556",
+                       Username = "marcus",
+                       Password = "secret",
+                       Claims = { new Claim("role", "user") },
+                   },
+                   new TestUser
+                   {
+                       SubjectId = "EFG-456",
+                       Username = "mary",
+                       Password = "secret",
+                       Claims = { new Claim("role", "editor") },
+                   },
+                   new TestUser
+                   {
+                       SubjectId = "HIJ-789",
+                       Username = "admin",
+                       Password = "secret",
+                       Claims = { new Claim("role", "admin") },
+                   },
+               };
+    }
+}
--- SampleAuthPolicies/Startup.cs
+++ SampleAuthPolicies/Startup.cs
@@ -1,8 +1,3 @@
-using System.Collections.Generic;
-using System.Security.Claims;
-using IdentityServer4.AccessTokenValidation;
-using IdentityServer4.Models;
-using IdentityServer4.Test;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,60 +10,9 @@ namespace SampleAuthPolicies
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddIdentityServer()
-               .AddInMemoryApiResources(new ApiResource[]
-               {
-                   new ApiResource("foo-api")
-                   {
-                       Scopes =
-                       {
-                           new Scope("foo-api.with.roles", new[] { "role" }),
-                       }
-                   }
-               })
-               .AddInMemoryClients(new[]
-               {
-                    new Client
-                    {
-                        // Don't use RPO if you can prevent it. We use it here
-                        // because it's the easiest way to demo with users.
-                        ClientId = "legacy-rpo",
-                        AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
-                        AllowAccessTokensViaBrowser = false,
-                        RequireClientSecret = false,
-                        AllowedScopes = { "foo-api", "foo-api.with.roles" },
-                    },
-               })
-               .AddTestUsers(new List<TestUser>
-               {
-                   new TestUser
-                   {
-                       SubjectId = "ABC-123",
-                       Username = "john",
-                       Password = "secret",
-                       Claims = { new Claim("role", "user") },
-                   },
-                   new TestUser
-                   {
-                       SubjectId = "ABC-556",
-                       Username = "marcus",
-                       Password = "secret",
-                       Claims = { new Claim("role", "user") },
-                   },
-                   new TestUser
-                   {
-                       SubjectId = "EFG-456",
-                       Username = "mary",
-                       Password = "secret",
-                       Claims = { new Claim("role", "editor") },
-                   },
-                   new TestUser
-                   {
-                       SubjectId = "HIJ-789",
-                       Username = "admin",
-                       Password = "secret",
-                       Claims = { new Claim("role", "admin") },
-                   },
-               })
+               .AddInMemoryApiResources(IdentityServerConfig.ApiResources)
+               .AddInMemoryClients(IdentityServerConfig.Clients)
+               .AddTestUsers(IdentityServerConfig.TestUsers)
                .AddDeveloperSigningCredential();
 
             services.AddAuthentication("Bearer")
@@ -76,7 +20,6 @@ namespace SampleAuthPolicies
                 {
                     options.Authority = "https://localhost:5001";
                     options.ApiName = "foo-api";
-                    options.SupportedTokens = SupportedTokens.Jwt;
                 });
 
             services.AddSingleton<IAuthorizationHandler, BlacklistUsersHandler>();
